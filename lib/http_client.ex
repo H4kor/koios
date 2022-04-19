1b9c8a79d48b379087f43a5fd3d1675e49ea4de2
Niko Abeler <niko@rerere.org>
2022-04-19 20:54:02 +0200

    initial commit. WIP Finder

---
+++ lib/http_client.ex
@@ -0,0 +1,19 @@
+defmodule HTTPClient do
+  use Tesla
+
+  plug Tesla.Middleware.FollowRedirects, max_redirects: 10
+
+  @spec get_page(String.t) :: {:ok, String.t} | {:error, any}
+  def get_page(url) do
+    request = get(url)
+    case request do
+      {:ok, response} ->
+        if (response.status >= 200 && response.status < 300 )do
+          {:ok, response.body}
+        else
+          {:error, {:status, response.status}}
+        end
+      {:error, error} -> {:error, error}
+    end
+  end
+end
