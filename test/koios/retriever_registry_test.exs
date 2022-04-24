ce6b706dc41f687b997c544f836e6668f83a6d8a
Niko Abeler <niko@rerere.org>
2022-04-24 11:24:20 +0200

    test recreation of retrievers

--- test/koios/retriever_registry_test.exs
+++ test/koios/retriever_registry_test.exs
@@ -2,11 +2,6 @@ defmodule Koios.RetrieverRegistryTest do
   use ExUnit.Case, async: true
   doctest Koios.RetrieverRegistry
 
-  # setup do
-  #   {:ok, retriever_registry} = Koios.RetrieverRegistry.start_link([])
-  #   %{retriever_registry: retriever_registry}
-  # end
-
   test "get retriever returns same retriever for two domains" do
     result_1 = Koios.RetrieverRegistry.get_retriever("http://www.example.com")
     result_2 = Koios.RetrieverRegistry.get_retriever("http://www.example.com/foo")
@@ -18,5 +13,13 @@ defmodule Koios.RetrieverRegistryTest do
     result_2 = Koios.RetrieverRegistry.get_retriever("http://www.foobar.com")
     assert result_1 != result_2
   end
+
+  test "removes registries on exit" do
+    first_retriever = Koios.RetrieverRegistry.get_retriever("http://www.example.com")
+    GenServer.stop(first_retriever)
+    second_retriever = Koios.RetrieverRegistry.get_retriever("http://www.example.com")
+    assert first_retriever != second_retriever
+  end
+
 
 end
